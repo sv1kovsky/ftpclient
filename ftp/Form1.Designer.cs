53d878ab154fb1aabaa532f531f6495e7c6fee71
sv1kovsky <bi50_s.a.velichko@mpt.ru>
2020-03-27 15:55:27 +0300

    fc

---
+++ ftp/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace ftp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textHost = new System.Windows.Forms.TextBox();
+            this.textUser = new System.Windows.Forms.TextBox();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textHost
+            // 
+            this.textHost.Location = new System.Drawing.Point(12, 45);
+            this.textHost.Name = "textHost";
+            this.textHost.Size = new System.Drawing.Size(172, 22);
+            this.textHost.TabIndex = 0;
+            this.textHost.Text = "95.165.158.197";
+            // 
+            // textUser
+            // 
+            this.textUser.Location = new System.Drawing.Point(190, 45);
+            this.textUser.Name = "textUser";
+            this.textUser.Size = new System.Drawing.Size(172, 22);
+            this.textUser.TabIndex = 1;
+            this.textUser.Text = "Sergey";
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(368, 45);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(172, 22);
+            this.textPassword.TabIndex = 2;
+            this.textPassword.Text = "12345";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Host";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(187, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(365, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(553, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(16, 160);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(671, 273);
+            this.treeView1.TabIndex = 7;
+            this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Status:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(71, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(197, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Соединение не установлено";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(306, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.textUser);
+            this.Controls.Add(this.textHost);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textHost;
+        private System.Windows.Forms.TextBox textUser;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ ftp/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+
+namespace ftp
+{
+    public partial class Form1 : Form
+    {
+        //поле для хранения имени фтп-сервера
+        private string _Host;
+
+        //поле для хранения логина
+        private string _UserName;
+
+        //поле для хранения пароля
+        private string _Password;
+
+        //объект для запроса данных
+        FtpWebRequest ftpRequest;
+
+        //объект для получения данных
+        FtpWebResponse ftpResponse;
+        //поле для хранения пути
+        private string path = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void InitConnectData()
+        {
+            _Host = textHost.Text;
+            _UserName = textUser.Text;
+            _Password = textPassword.Text;
+        }
+
+        private void loadtree(FtpWebResponse ftpResponse)
+        {
+            StreamReader sr = new StreamReader(ftpResponse.GetResponseStream(), System.Text.Encoding.ASCII);
+            string line;
+            while ((line = sr.ReadLine()) != null) //читаем по одной линии(строке) пока не вычитаем все из потока (пока не достигнем конца файла)
+            {
+                treeView1.Nodes.Add(line);
+            }
+            sr.Close();
+            ftpResponse.Close();
+        }
+
+        private void FtpConnect(string path)
+        {
+            label6.Text = path;
+            treeView1.Nodes.Clear();
+            //Создаем объект запроса
+            ftpRequest = (FtpWebRequest)WebRequest.Create("ftp://" + _Host + path);
+            //логин и пароль
+            ftpRequest.Credentials = new NetworkCredential(_UserName, _Password);
+            //команда фтп LIST
+            ftpRequest.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+            ftpRequest.EnableSsl = false;
+            //Получаем входящий поток
+            try
+            {
+                ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+                label5.Text = "Соединение установлено";
+                loadtree(ftpResponse);
+            }
+            catch (WebException)
+            {
+                label5.Text = "Соединение не удалось";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textHost.Enabled = false;
+            textUser.Enabled = false;
+            textPassword.Enabled = false;
+            button1.Enabled = false;
+            InitConnectData();
+            FtpConnect(path);
+        }
+
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if(treeView1.SelectedNode.Text.Substring(24, 5) == "<DIR>")
+            {
+                string papka;
+                papka = treeView1.SelectedNode.Text.Substring(39, treeView1.SelectedNode.Text.Length - 39);
+                path += '/' + papka;
+                FtpConnect(path);
+            }
+        }
+    }
+}
